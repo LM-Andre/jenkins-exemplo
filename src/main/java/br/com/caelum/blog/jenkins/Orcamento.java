70b10b43fa3fb143af905277ad05e32a846c246f
LM-Andre <aminozzi@leroymerlin.com.br>
2016-03-10 11:00:32 -0300

    test2

--- src/main/java/br/com/caelum/blog/jenkins/Orcamento.java
+++ src/main/java/br/com/caelum/blog/jenkins/Orcamento.java
@@ -4,15 +4,22 @@ public class Orcamento
 {
 
 	private final double valor;
+	private final Long id;
 
-	public Orcamento(final double valor)
+	public Orcamento(final double valor, final Long id)
 	{
 		this.valor = valor;
+		this.id = id;
 	}
 
 	public double getValor()
 	{
 		return valor;
+	}
+
+	public Long getId()
+	{
+		return id;
 	}
 
 }
